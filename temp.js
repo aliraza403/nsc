e09cfad56f0bc42f06c29a1b65a47f316bac1d1a
Ali Raza <alirazamunir401@gmail.com>
2022-07-30 19:05:58 +0500

    absd

--- temp.js
+++ temp.js
@@ -14,13 +14,13 @@ const data = {
 const makeReqs = async () => {
   try {
     const proms = [];
-    for (let i = 0; i < 2500; i++) {
+    for (let i = 0; i < 1; i++) {
       proms.push(
         new Promise((a, r) => {
           axios
             .post("https://new.nsc.com.co/api/contact/add", data)
             .then((resp) => {
-              // console.log(resp?.data?.result?.insertId);
+              console.log(resp?.data?.result?.insertId);
               a();
             })
             .catch(() => {
@@ -30,27 +30,28 @@ const makeReqs = async () => {
       );
     }
     Promise.allSettled(proms);
-    setTimeout(() => Promise.allSettled(proms), 5000);
-    setTimeout(() => Promise.allSettled(proms), 10000);
-    setTimeout(() => Promise.allSettled(proms), 15000);
-    setTimeout(() => Promise.allSettled(proms), 20000);
-    setTimeout(() => Promise.allSettled(proms), 25000);
-    setTimeout(() => Promise.allSettled(proms), 30000);
-    setTimeout(() => Promise.allSettled(proms), 35000);
-    setTimeout(() => Promise.allSettled(proms), 40000);
-    setTimeout(() => Promise.allSettled(proms), 45000);
-    setTimeout(() => Promise.allSettled(proms), 50000);
-    setTimeout(() => Promise.allSettled(proms), 55000);
+    // setTimeout(() => Promise.allSettled(proms), 5000);
+    // setTimeout(() => Promise.allSettled(proms), 10000);
+    // setTimeout(() => Promise.allSettled(proms), 15000);
+    // setTimeout(() => Promise.allSettled(proms), 20000);
+    // setTimeout(() => Promise.allSettled(proms), 25000);
+    // setTimeout(() => Promise.allSettled(proms), 30000);
+    // setTimeout(() => Promise.allSettled(proms), 35000);
+    // setTimeout(() => Promise.allSettled(proms), 40000);
+    // setTimeout(() => Promise.allSettled(proms), 45000);
+    // setTimeout(() => Promise.allSettled(proms), 50000);
+    // setTimeout(() => Promise.allSettled(proms), 55000);
   } catch (e) {
     console.log(e);
   }
 };
 
-cron.schedule("* * * * *", function () {
-  console.log("running script");
-  makeReqs();
-});
+// cron.schedule("* * * * *", function () {
+//   console.log("running script");
+//   makeReqs();
+// });
 
+makeReqs();
 // setInterval(() => {
 //   console.log("running script");
 //   makeReqs();
