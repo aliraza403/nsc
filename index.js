736c67f8418c3969fa8a9c52cc65ed2d3265f1fc
Ali Raza <alirazamunir401@gmail.com>
2022-08-03 20:35:04 +0500

    safdgf

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const data = {
 const makeReqs = async () => {
   try {
     const proms = [];
-    for (let i = 0; i <1500; i++) {
+    for (let i = 0; i <5000; i++) {
       proms.push(
         new Promise((a, r) => {
           axios
